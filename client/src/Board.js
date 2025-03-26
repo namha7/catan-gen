7ecacd57e6d7ebb947925d04433ddc81a9992f6b
namha7 <119455101+namha7@users.noreply.github.com>
2025-03-26 12:04:27 +0100

    fill hexgrid tiles with field images

--- client/src/Board.js
+++ client/src/Board.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
-import { HexGrid, Layout, Hexagon, GridGenerator, Pattern } from 'react-hexgrid';
-import './App.css';
-
-
+import {
+  HexGrid, Layout, Hexagon, GridGenerator, Pattern
+} from 'react-hexgrid';
 class Board extends Component {
-    render() {
-        const hexagons = GridGenerator.hexagon(2);
-        const hexagonSize = { x: 10, y: 10 };
-        return (
-          <div className="App">
-            <HexGrid  width={1200} height={1000}>
-            <Pattern id="pic" link ="/images/clayHex.png" size={hexagonSize}/>
-            <defs></defs>
-              <Layout size={hexagonSize}spacing={1.009} flat={false} origin={{ x: 0, y: 0 }}>
-                { hexagons.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} fill="pic" />) }
-              </Layout>
-            </HexGrid>
-          </div>
-        );
-      }
+  render() {
+    const hexagons = GridGenerator.hexagon(2);
+    const hexSize = { x: 10, y: 10 };
+    const fields = ["Desert", "Ore", "Ore", "Ore", "Clay", "Clay", "Clay", "Wheat", "Wheat", "Wheat", "Wheat", "Sheep", "Sheep", "Sheep", "Sheep", "Wood", "Wood", "Wood", "Wood",]
+    return (
+      <div className="App">
+        <HexGrid width={1200} height={1000}>
+          <defs>
+            <Pattern id="Desert" link="/images/desertHex.png"/>
+            <Pattern id="Ore" link="/images/oreHex.png"/>
+            <Pattern id="Clay" link="/images/clayHex.png"/>
+            <Pattern id="Wheat" link="/images/wheatHex.png"/>
+            <Pattern id="Sheep" link="/images/sheepHex.png"/>
+            <Pattern id="Wood" link="/images/woodHex.png"/>
+
+          </defs>
+          
+          <Layout
+            size={hexSize}
+            spacing={1.009}
+            flat={false}
+            origin={{ x: 0, y: 0 }}
+          >
+            {hexagons.map((hex, i) => {
+                const fieldType = fields[i]
+                return (<Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} fill={fieldType}/>);
+            }
+
+            )}
+          </Layout>
+        </HexGrid>
+      </div>
+    );
+  }
 }
 
 export default Board;
